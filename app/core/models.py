4d28eaa9b2e1306306956e61e0618f635cb26de9
Alper <itsc0508@gmail.com>
2022-05-08 09:33:30 +0300

    added custom user model & refactoring

---
+++ app/core/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        """Creates and saves a new user"""
+        if not email:
+            raise ValueError(
+                "Email cannot be empty! Please pass an email address to create th user."
+            )
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Creates and saves a new super user"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Custom user model that supports email addresses instead of username"""
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    photo = models.ImageField(null=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
---
+++ app/core/tests/test_models.py
@@ -0,0 +1,69 @@
+from django.test import TestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.contrib.auth import get_user_model
+
+import os
+import filecmp
+
+
+class ModelTests(TestCase):
+    def test_create_user_with_email_successful(self):
+        """Test creating a new user with an email is successful"""
+        email = "test@test.com"
+        password = "TestPassword123"
+        user = get_user_model().objects.create_user(email=email, password=password)
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_create_user_with_email_and_photo_successful(self):
+        """Test creating a new user with an email is successful"""
+        email = "test@test.com"
+        password = "TestPassword123"
+        
+        with open("./assets_test/1.jpg", "rb") as f:
+            img_file = f.read()
+        
+        photo = SimpleUploadedFile(
+            name="test_image.jpg",
+            content=img_file,
+            content_type="image/jpeg",
+        )
+        
+        user = get_user_model().objects.create_user(
+            email=email, password=password, photo=photo
+        )
+
+        with open("uploaded_photo.jpg", "wb") as f:
+            f.write(img_file)
+        
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+        self.assertTrue(filecmp.cmp("uploaded_photo.jpg", "test_image.jpg"))
+        
+
+        os.remove("test_image.jpg")
+        os.remove("uploaded_photo.jpg")
+
+    def test_email_is_normalized(self):
+        """Test creating a new user with email that contains uppercase case characters"""
+        email = "test634@TeSt.com"
+        password = "TestPassword123"
+        user = get_user_model().objects.create_user(email=email, password=password)
+
+        self.assertEqual(user.email, email.lower())
+        self.assertTrue(user.check_password(password))
+
+    def test_create_user_with_invalid_email(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(email=None, password="test1235Tsert")
+
+    def test_create_new_super_user(self):
+        """Text creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            "test@test.com",
+            "test123",
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
