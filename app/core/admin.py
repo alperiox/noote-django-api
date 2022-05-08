07be6133831bc13c19d51a5a874aa682902559fb
Alper <itsc0508@gmail.com>
2022-05-08 22:14:53 +0300

    black: refactored

--- app/core/admin.py
+++ app/core/admin.py
@@ -8,23 +8,24 @@ from core import models
 
 
 class UserAdmin(BaseUserAdmin):
-    ordering = ['id']
-    list_display = ["email", 'name']
+    ordering = ["id"]
+    list_display = ["email", "name"]
     fieldsets = (
         (None, {"fields": ("email", "password")}),
         (_("Personal Info"), {"fields": ("name", "photo")}),
-        (
-            _("Permissions"),
-            {"fields": ("is_active", "is_staff", "is_superuser")}
-        ), 
-        (_("Important dates"), {"fields": ("last_login", )})
+        (_("Permissions"), {"fields": ("is_active", "is_staff", "is_superuser")}),
+        (_("Important dates"), {"fields": ("last_login",)}),
     )
 
     add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2', 'photo')
-        }),
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2", "photo"),
+            },
+        ),
     )
+
 
 admin.site.register(models.User, UserAdmin)
